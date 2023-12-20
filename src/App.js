dc8accdc66b8c24111c90a984b720aef600d663d
Priyanka8446 <priyankanimje49@gmail.com>
2023-12-20 11:53:18 +0530

    'project-updated'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import "./App.css";
 import Home from "./component/Home";
-import { useState } from "react";
-import Filter from "./component/Filter";
-
-// export const globalData = createContext();
 
 function App() {
-  // const [newData, setNewData] = useState(null)
-  // const [createData, setCreateData] = useState([]);
-
-  // const globalData = createContext()
-
-  // const[newData, setNewData] = useState([])
-
-  // const setCreateData=(item)=>{
-  //   setNewData((prev)=>[...prev, item])
-  // }
+  
 
   return (
     <>
       <Home/>
-      {/* <Filter setNewData={setNewData} newData={newData}/> */}
     </>
   );
 }
